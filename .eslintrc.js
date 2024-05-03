22d3f2fa615502201c5c89390c8bb3f51ed126d0
hiTushar <tusharjain2201@gmail.com>
2024-05-03 18:00:28 +0530

    Initial project

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+  },
+};
